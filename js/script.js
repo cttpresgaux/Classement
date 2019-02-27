85444fc7e70be9612a614d3c89f1afc6c056c624
Emeric Beauchot <emeric.beauchot@gmail.com>
2019-02-27 14:35:58 +0100

    Update script.js
    erreur premier match WO non present dans les données

--- js/script.js
+++ js/script.js
@@ -553,7 +553,7 @@ function setMatchesByDiv(resp, callback) {
 
                     for (var i4 = 1; i4 < Matchs.length; i4++) {
                         var m = Matchs[i4];
-                        if (!m.wo) {
+                        if (m != undefined && !m.wo) {
                             if (m.homeWon) {
                                 HPs[m.hpi].won.push(APs[m.api].ranking);
                                 APs[m.api].lost.push(HPs[m.hpi].ranking);
